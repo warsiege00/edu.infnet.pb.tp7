7645846e472a22490b923e3e67eb30cef2a90c14
Matheus Piraine <matheus.almeida053@gmail.com>
2024-04-06 00:03:07 -0300

    fix: Ajuste classe aluno

--- pb.tp7/Models/Aluno.cs
+++ pb.tp7/Models/Aluno.cs
@@ -4,10 +4,10 @@ namespace pb.tp7.Models
 	public class Aluno
 	{
 		public int matricula { get; set; }
-        public string nome { get; set; }
+        	public string nome { get; set; }
 		public Turma turma { get; set; }
 
-        public Aluno(int matricula, string nome)
+	        public Aluno(int matricula, string nome)
 		{
 			this.matricula = matricula;
 			this.nome = nome;
